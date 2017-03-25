80f9d01f3ce37693ad9d3e5a2335596904d73e2e
lfbandeira <lfbandeira@trt13.jus.br>
2017-03-24 23:30:17 -0300

    First commit

---
+++ src/br/com/flightsmonitor/voegol/exception/FlightNotFoundException.java
@@ -0,0 +1,14 @@
+package br.com.flightsmonitor.voegol.exception;
+
+public class FlightNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public FlightNotFoundException(Exception e) {
+		super(e);
+	}
+	
+	public FlightNotFoundException(String message) {
+		super(message);
+	}
+}
