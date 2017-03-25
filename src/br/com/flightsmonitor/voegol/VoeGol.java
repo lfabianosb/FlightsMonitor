dd8d6511588eb4a28ec048ab49eb534e4d1f2468
lfbandeira <lfbandeira@trt13.jus.br>
2017-03-24 23:46:05 -0300

    Corrigindo imports

--- src/br/com/flightsmonitor/voegol/VoeGol.java
+++ src/br/com/flightsmonitor/voegol/VoeGol.java
@@ -3,15 +3,10 @@ package br.com.flightsmonitor.voegol;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
